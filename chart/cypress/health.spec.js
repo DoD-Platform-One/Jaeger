51903d6b0496b46760bfa9ab80eb55b503a9571f
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-10 16:48:21 -0400

    add helm test for tracing

--- chart/cypress/health.spec.js
+++ chart/cypress/health.spec.js
@@ -1,5 +1,11 @@
 describe('Basic', function() {
   it('Check UI is accessible', function() {
       cy.visit(Cypress.env('url'))
+      cy.title().should("eq", "Jaeger UI");
+      cy.get('.ant-form-item-control-wrapper:first').click().type('{enter}');
+             
+      
+      cy.get('.ant-btn').click();
+     cy.get('.SearchResults--header').should("be.visible")
   })
 })
