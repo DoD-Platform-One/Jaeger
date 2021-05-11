25372bd273b21af983b59abf8702f37cc1489c0e
Micah Nagel <micah.nagel@parsons.com>
2021-05-11 10:40:39 -0600

    update to use test library

--- chart/tests/cypress/health.spec.js
+++ chart/tests/cypress/health.spec.js
@@ -4,6 +4,6 @@ describe('Basic', function() {
       cy.title().should("eq", "Jaeger UI");
       cy.get('.ant-form-item-control-wrapper:first').click().type('{enter}');
       cy.get('.ant-btn').click();
-     cy.get('.SearchResults--header').should("be.visible")
+      cy.get('.SearchResults--header').should("be.visible")
   })
 })
