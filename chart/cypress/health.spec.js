f3a45d6545e8ab70f2112f8e3c1cb70bb44506c4
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-11 10:05:49 -0400

    add helm test

--- chart/cypress/health.spec.js
+++ chart/cypress/health.spec.js
@@ -3,8 +3,6 @@ describe('Basic', function() {
       cy.visit(Cypress.env('url'))
       cy.title().should("eq", "Jaeger UI");
       cy.get('.ant-form-item-control-wrapper:first').click().type('{enter}');
-             
-      
       cy.get('.ant-btn').click();
      cy.get('.SearchResults--header').should("be.visible")
   })
